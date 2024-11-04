e6245875bc9db315db56735ca9b7da73804b9e0e
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-11-04 14:29:03 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Search from './pages/Search'
 import MyAccount from './pages/myAccount'
+import { useState } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -18,8 +19,11 @@ const App = () => {
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><input defaultValue={'Search'}></input></li>
+            <li><Link to="/my-account">My Account</Link></li>
             </ul>
         </div>
+
+{/* Navbar present for now. LI/UL added to tie it to CSS for the time. */}
 
       <Routes>
         <Route path='/' element={<Welcome />}></Route>
