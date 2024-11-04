cc2e0acbd8a128c1d0b054823ccbc83c098b3c16
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-11-04 14:05:43 +0000

    added routes and search and my account files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Welcome from './pages/Welcome'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Search from './pages/Search'
+import MyAccount from './pages/myAccount'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <Routes>
+        <Route path='/' element={<Welcome />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/search' element={<Search />}></Route>
+        <Route path='/my-account' element={<MyAccount />}></Route>
+      </Routes>
     </div>
   )
 }
