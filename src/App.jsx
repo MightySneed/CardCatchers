df36938dd17a0de06e1fc3cdeb5ffc8a19d2758e
Mark McCarthy <mm3guides@gmail.com>
2024-11-04 12:18:25 +0000

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mark McCarthy</h1>
+      <h1>Marks Change</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
