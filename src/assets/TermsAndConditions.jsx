9eb9ab321f8ff1e53bdbec1c1581ddf8ff72214e
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-11-06 12:07:12 +0000

    added some functionality for updating details and began to type out the terms and conditions.

---
+++ src/assets/TermsAndConditions.jsx
@@ -0,0 +1,16 @@
+const TCs = () => {
+    return (
+        <div>
+            <h1>TERMS AND CONDITIONS</h1>
+            <h5>Last Updated 6th November 2024</h5>
+            <br />
+            <h2>AGREEMENT TO OUR LEGAL TERMS</h2>
+            <p>We are TCG Card Catchers('Company', 'We', 'Us', or 'Our'), a company registered in the United Kingdom at 28 Paradise Street, Liverpool, UK, L1 3EU.</p>
+            <p>We operate the website [enter web address here once hosted] (the 'Site'), as well as any other related products and services that refer or link to these legal terms (the 'Legal Terms')(collectively, the 'Services').</p>
+            <p>You can contact us by phone at 0151 236 0070, email at admin@tcgcc.co.uk, or by post to 28 Paradise Street, Liverpool, United Kingdom, L1 3EU.</p>
+            <p>These legal terms constitute a legally binding agreement made between you, whether personally and/or on behalf of an entity ('you'), and TCG Card Catchers, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSELY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
+        </div>
+    )
+}
+
+export default TCs
