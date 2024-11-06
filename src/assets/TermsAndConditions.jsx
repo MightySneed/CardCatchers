1d5e8286f260da6e1171ccd0aaa29e55b52ea8f3
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-11-06 12:59:30 +0000

    continued typing up the terms and conditions. yet to be completed.

--- src/assets/TermsAndConditions.jsx
+++ src/assets/TermsAndConditions.jsx
@@ -9,6 +9,15 @@ const TCs = () => {
             <p>We operate the website [enter web address here once hosted] (the 'Site'), as well as any other related products and services that refer or link to these legal terms (the 'Legal Terms')(collectively, the 'Services').</p>
             <p>You can contact us by phone at 0151 236 0070, email at admin@tcgcc.co.uk, or by post to 28 Paradise Street, Liverpool, United Kingdom, L1 3EU.</p>
             <p>These legal terms constitute a legally binding agreement made between you, whether personally and/or on behalf of an entity ('you'), and TCG Card Catchers, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSELY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
+            <p>We will provide you with prior notice of any scheduled changes to the Services you are using. The modified Legal Terms will become effective upon posting or notifying you by admin@tcgcc.co.uk, as stated in the email message. By continuing to use the Services after the effective date of any changes, you agree to be bound by the modified terms.</p>
+            <p>All users who are minors in the jurisdiction of which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Services. If you are a minor, ou must have your parent or guardian read and agree to these Legal Terms prior to you using the Services.</p>
+            <p>We recommend printing a copy of these Legal Terms for your records.</p>
+            <h2>Table Of Contents</h2>
+            <p>content table goes here with links to each individual section</p>
+            <h2>1. OUR SERVICES</h2>
+            <p>The information provided when using the Services is not intended for distribution to or use by any person and/or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Services from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.</p>
+            <p>The Services are not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPPA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use the Services. You may not use the services in any way that would violate the Gramm-Leach-Bliley Act (GLBA).</p>
+            <h2>2. INTELLECTUAL PROPERTY RIGHTS</h2>
         </div>
     )
 }
