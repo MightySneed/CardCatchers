7ff26ec2fbe728bb14017bc87bf4c94c01730365
MightySneed <179814389+MightySneed@users.noreply.github.com>
2024-11-07 14:25:05 +0000

    Merged now. As in previous one. Game Select changes the context of the searchbar. Searchbar appears and displays within the body. Routing needs fixing post-haste

---
+++ src/pages/MyCollections.jsx
@@ -0,0 +1,11 @@
+const MyCollections = () => {
+    return (
+        <div>
+            <h1>Your Collections</h1>
+            <button>New Collection</button>
+            <button>View All Collections</button>
+
+        </div>
+    )
+}
+export default MyCollections
