8e4a4901bfa3bde887113f81bc10b773596ce970
Jay <179924362+CedricSmith00@users.noreply.github.com>
2024-11-08 12:26:34 +0000

    Updated colour of Header and Footer as text was hard to read. Resized main page images so they are uniform.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,23 +1,24 @@
 import YGO from '../assets/YGO.jpg'
-import Pokemon from '../assets/Pokemon.jpg'
+import Pokemon from "../assets/Pokemon.jpg"
 import MTG from '../assets/MTG.jpg'
 
 const Welcome = () => {
     return (
-        <div class="image-float-container row">
-            <div  class="column img-colour" id='yugioh'>
-                <img  src={YGO} alt="Yu-Gi-h! Trading Card Game" />
-                <h2>Yu-Gi-Oh!</h2>
-            </div>
-            <div class="column img-colour" id='pokemon'>
-                <img src={Pokemon} alt="Pokemon Trading Card Game" />
-                <h2>Pokemon</h2>
-            </div>
-            <div class="column img-colour" id='magic'>
-                <img src={MTG} alt="Magic the Gathering Trading Card Game" />
-                <h2>Magic the Gathering</h2>
-            </div>
+        <div>
+        <div className="img-colour img-size" id='yugioh'>
+            <img src={YGO} alt="Yu-Gi-h! Trading Card Game" className='mainImg'/>
         </div>
+        <br />
+        <br />
+        <div className="img-colour "id='pokemon'>
+            <img src={Pokemon} alt="Pokemon Trading Card Game" className='mainImg'/>
+        </div>
+        <br />
+        <br />
+        <div className="img-colour "id='magic'>
+            <img src={MTG} alt="Magic the Gathering Trading Card Game" className='mainImg'/>
+        </div>
+    </div>
     )
 }
 export default Welcome
