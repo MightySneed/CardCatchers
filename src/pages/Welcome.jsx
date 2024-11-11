8b60c5f8012a7255b00ff242ee2a108c92bc5554
Jay Roach <179924362+CedricSmith00@users.noreply.github.com>
2024-11-11 12:34:44 +0000

    The images on the main screen have been turned into links to the search page.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,22 +1,29 @@
 import YGO from '../assets/YGO.jpg'
 import Pokemon from "../assets/Pokemon.jpg"
 import MTG from '../assets/MTG.jpg'
+import { Link } from "react-router-dom"
 
 const Welcome = () => {
     return (
         <div class>
         <div className="img-container" id='yugioh'>
+        <Link to="/search">
             <img src={YGO} alt="Yu-Gi-h! Trading Card Game" className='mainImg img-size img-colour'/>
+            </Link>
         </div>
         <br />
         <br />
         <div className="img-container"id='pokemon'>
+        <Link to="/search">
             <img src={Pokemon} alt="Pokemon Trading Card Game" className='mainImg img-size img-colour'/>
+        </Link>
         </div>
         <br />
         <br />
         <div className="img-container"id='magic'>
+        <Link to="/search">
             <img src={MTG} alt="Magic the Gathering Trading Card Game" className='mainImg img-size img-colour'/>
+        </Link>
         </div>
     </div>
     )
