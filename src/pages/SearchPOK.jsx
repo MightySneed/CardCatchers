2bfb7901c024a231e6182469bdd4792ff6bf5869
rukayat okulaja <rukayatokulaja@yahoo.co.uk>
2024-11-13 12:43:37 +0000

    added image

--- src/pages/SearchPOK.jsx
+++ src/pages/SearchPOK.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { addToCollectionPOK } from '../utilities/addCollectionPOK';
 
 const SearchBarPOK = () => {
     const [cards, setCards] = useState([]);
