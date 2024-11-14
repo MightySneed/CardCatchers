8e6e1c07609c562a3006c3137210495e9d9e25f1
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-11-14 16:08:13 +0000

    delete account functionality added

--- src/utilities/deleteAccount.js
+++ src/utilities/deleteAccount.js
@@ -1,4 +1,29 @@
-const deleteAccount = () => {
+import readCookie from "./readCookie";
 
-}
-export default deleteAccount
+// deleteAccount.js
+const deleteAccount = async (username) => {
+    try {
+        const token = readCookie('jwt_token')
+        const res = await fetch('https://cardcatchersbackend-production.up.railway.app/deleteUser', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({ username: username }),
+        });
+        
+        if (!res.ok) {
+            throw new Error('Failed to delete user');
+        }
+
+        const result = await res.json();
+        console.log('User deleted successfully');
+        return result;
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+};
+
+export default deleteAccount;
